ad1fa842952b90d9ab4bf528f64827406ae0bd5c
Maksim Nemchenya <m.nemchenya@itransition.com>
2024-04-20 13:27:02 +0300

    Added entity

---
+++ AM.Cars.Server/AM.Cars.Server.Domain/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AM.Cars.Server.Domain.Entities;
+
+public class Car
+{
+    public long Id { get; set; }
+
+    [Required]
+    public string FileName { get; set; }
+
+    [Required]
+    public string ImagePath { get; set; }
+}
