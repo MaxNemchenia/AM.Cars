feb1c3385c3ab78a10e66abcbc73e9fe51a867af
Maksim Nemchenya <m.nemchenya@itransition.com>
2024-04-20 16:26:56 +0300

    Added validator

---
+++ AM.Cars.Server/AM.Cars.Server.Infrustructure/Validators/CarDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using AM.Cars.Server.Infrustructure.Dtos;
+using FluentValidation;
+
+namespace AM.Cars.Server.Infrustructure.Validators;
+
+public class CarDtoValidator : AbstractValidator<CarDto>
+{
+    public CarDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .MaximumLength(255).WithMessage("Name length cannot exceed 255 characters");
+
+        RuleFor(x => x.Image).NotNull().WithMessage("Image is required");
+    }
+}
