b0d136f2b325948830f7c5546ebfb11ccd2eeb88
Maksim Nemchenya <m.nemchenya@itransition.com>
2024-04-20 13:30:19 +0300

    Added Migrations

---
+++ AM.Cars.Server/AM.Cars.Server.Infrustructure.Database/Migrations/20240420102425_SetupDatabase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AM.Cars.Server.Infrustructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetupDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
