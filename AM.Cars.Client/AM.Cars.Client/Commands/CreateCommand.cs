d656d3a651eaf6ee9fa36c24605cb5146ca7e413
Maksim Nemchenya <m.nemchenya@itransition.com>
2024-04-23 12:52:45 +0300

    Added commands

---
+++ AM.Cars.Client/AM.Cars.Client/Commands/CreateCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace AM.Cars.Client.Commands;
+
+public class CreateCommand : ICommand
+{
+    public event EventHandler CanExecuteChanged;
+
+    private readonly Action _execute;
+
+    public CreateCommand(Action execute) => _execute = execute;
+
+    public bool CanExecute(object parameter) => true;
+
+    public void Execute(object parameter)
+    {
+        _execute();
+    }
+}
+
