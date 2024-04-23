98ffbe6cb804ee4d837a4158df9eec8d97e14871
Maksim Nemchenya <m.nemchenya@itransition.com>
2024-04-23 12:52:26 +0300

    Added Converters

---
+++ AM.Cars.Client/AM.Cars.Client/Converters/ValueConverters/Base64ToImageConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace AM.Cars.Client.Converters.ValueConverters;
+
+public class Base64ToImageConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string base64String)
+        {
+            var binaryData = System.Convert.FromBase64String(base64String);
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = new MemoryStream(binaryData);
+            bitmapImage.EndInit();
+
+            return bitmapImage;
+        }
+
+        return default;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
